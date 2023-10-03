c367fff46da923de7a99a3e82185f30543359597
Landon <landoncramer1@gmail.com>
2023-10-03 06:12:10 -0700

    practice 2

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,9 @@ import { image } from "../data/data";
 function About() {
   return <div id="about">
     <h2>About Me</h2>
-    <p> Landon Cramer</p>
+    <p>Hello World</p>
     <img src={image} alt="I made this" />
-    </div>;
+  </div>;
 }
 
 export default About;
